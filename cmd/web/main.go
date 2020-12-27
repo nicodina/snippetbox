6e756deb97ddbcb8eb89ae7ef4f4117a0c038667
ndinardo <nicola.dinardo@ticketeasy.it>
2020-12-27 16:36:10 +0100

    Adding style + static file server + making server address configurable

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
 
+	addr := flag.String("addr", ":8080", "HTTP network address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	log.Println("Starting server on :8080")
-	err := http.ListenAndServe(":8080", mux)
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	log.Println("Starting server on ", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	
 	log.Fatal(err.Error())
 }
