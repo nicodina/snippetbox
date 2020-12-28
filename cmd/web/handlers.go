fbba23db5280285a74908e522e069db4ed46fdaf
ndinardo <nicola.dinardo@ticketeasy.it>
2020-12-28 17:43:38 +0100

    Adding router

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,10 +9,6 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request){
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
 
 	snippets, err := app.snippets.Latest()
 	if err != nil {
@@ -25,7 +21,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request){
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request){
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -44,12 +40,11 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request){
 	app.render(w, r, "show.page.html", data)
 }
 
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Create a new snippet ..."))
+}
+
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request){
-	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
 
 	// Example of a new snippet
 	title := "0 Snails"
@@ -62,5 +57,5 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request){
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
