eb3e2e2a83a28569c3365685df22d6d7006f5378
ndinardo <nicola.dinardo@ticketeasy.it>
2020-12-30 10:29:32 +0100

    Snippet creation flow

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -41,16 +41,23 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request){
 }
 
 func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Create a new snippet ..."))
+	app.render(w, r, "create.page.html", nil)
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request){
 
-	// Example of a new snippet
-	title := "0 Snails"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi"
-	expires := "7"
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 
+	// Retrieving form data
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	expires := r.PostForm.Get("expires")
+
+	// Create a new snippet in the database
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
 		app.serverError(w, err)
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/bmizerany/pat v0.0.0-20170815010413-6226ea591a40 // indirect
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/justinas/alice v1.2.0 // indirect
+	github.com/sqs/goreturns v0.0.0-20181028201513-538ac6014518 // indirect
 )
